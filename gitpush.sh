dca7a85f3892191c0003f7732b1d2d2af231129d
Sujoy Ghosal <sujoy.ghosal@gmail.com>
2019-03-01 09:19:59 -0500

    first commit

---
+++ gitpush.sh
@@ -0,0 +1,5 @@
+#Add following line if repository is existing
+git remote add origin https://github.com/sujoyghosal/PersonalityWeb.git
+git add .
+git commit -m "First Commit"
+git push -u origin master
